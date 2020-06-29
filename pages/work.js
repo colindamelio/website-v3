4350be9337688731e274be3854a477f6cfe1d484
Colin <colin.damelio@gmail.com>
2020-06-29 13:21:39 -0400

    cleanup

--- pages/work.js
+++ pages/work.js
@@ -1,66 +1,28 @@
-import Link from 'next/link';
-
-import styled from 'styled-components';
 import { motion } from 'framer-motion';
-import { fadeIn, stagger } from '../motions';
 
-const HomeButton = styled.a`
-  width: 10px;
-  height: 25px;
-  border-radius: 3px;
-  background: #fff;
-  color: red;
-  box-shadow: 0 11px 40px 0 rgba(0, 0, 0, 0.25), 0 2px 10px 0 rgba(0, 0, 0, 0.1);
-`;
+import { fadeIn, stagger } from '../motions';
+import projects from '../data/projects';
 
 const work = () => {
   return (
     <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
-      {/* <Link href="/" passHref>
-        <HomeButton>
-        </HomeButton>
-      </Link> */}
       <section>
         <motion.h1 variants={fadeIn}>Work</motion.h1>
         <motion.ul variants={stagger}>
-          <motion.li variants={fadeIn}>
-            <div>
-              <p>
-                <a href="https://www.honda.ca/buildyourhonda">
-                  Honda/Acura Build and Price
-                </a>{' '}
-                - Front-end Developer @ Climax Media
-              </p>
-            </div>
-          </motion.li>
-          <motion.li variants={fadeIn}>
-            <div>
-              <p>
-                <a href="https://www.honda.ca/dealerlocator">
-                  Honda/Acura Dealer Locator
-                </a>{' '}
-                - Front-end Developer @ Climax Media
-              </p>
-            </div>
-          </motion.li>
-          <motion.li variants={fadeIn}>
-            <div>
-              <p>
-                <a href="https://www.acura.ca">Acura Homepage</a> - Front-end
-                Developer @ Climax Media
-              </p>
-            </div>
-          </motion.li>
-          <motion.li variants={fadeIn}>
-            <div>
-              <p>
-                <a href="https://www.saraswatibali.com">
-                  Saraswati Bali Retreats
-                </a>{' '}
-                - Personal project
-              </p>
-            </div>
-          </motion.li>
+          {projects.map(work => {
+            return (
+              <motion.li variants={fadeIn}>
+                <div>
+                  <p>
+                    <a href={work.url}>
+                      {work.name}
+                    </a>{' '}
+                    - {work.tag}
+                  </p>  
+                </div>
+              </motion.li>
+            );
+          })}
         </motion.ul>
         <style jsx>{`
           section {
